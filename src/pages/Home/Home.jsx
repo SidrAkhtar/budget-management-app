06402a8c7de9d7fa7c4f6edd3165804673857146
sidrakhtar <reachsidraakhtar@gmail.com>
2022-09-14 01:57:36 -0700

    overall css touchup

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,8 +6,8 @@ export default function Home({ budget }) {
       <>
       <main className="Home-container">
          <div className="Home-h1">
-            <h2>Get Your Finances in Order</h2>
-            <h2>Click <strong>Add Budget</strong> to Create your budget and start tracking you expenses!</h2>
+            <h3>Get Your Finances in Order</h3>
+            <h3>Click <strong>Add Budget</strong> to Create your budget and start tracking you expenses!</h3>
          </div>
          <button>
             <Link to='/budget/new'>Add Budget</Link>
